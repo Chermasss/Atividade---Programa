775f0497461fd1727edaa475ee5b8d76dd7201d6
Chermasss <158324786+Chermasss@users.noreply.github.com>
2024-02-07 14:12:10 -0400

    soma.java

---
+++ soma.java
@@ -0,0 +1,20 @@
+import javax.swing.JOptionPane;
+
+public class Addition
+{
+    public static void main](String[] args ) 
+    {
+        String firstNumber =
+              JOptionPane.showInputDialog( "Enter first integer")
+        String secondNumber = 
+              JOptionPane.showInputDialog( "Enter second integer")
+                
+                
+        int number1 = integer.parseInt( firstName );
+        int number2 = integer.parseInt( secondName );
+        
+        int sum = number1 + number2;
+                
+                
+    }
+}
